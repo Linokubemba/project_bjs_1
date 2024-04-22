8ded403eb14da01b0c5432f417678b426eb74ecd
OnyvaStudio <onyva.studio64@gmail.com>
2024-04-22 14:55:34 +0200

    Add lightbulb

--- src/scenes/testScene.ts
+++ src/scenes/testScene.ts
@@ -1,3 +1,7 @@
+/*
+* STAIRCASE SWITCHES
+*/
+
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
 import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
@@ -21,6 +25,8 @@ import "@babylonjs/core/Culling/ray";
 
 // Assets
 import lightbulbModel from "../../assets/glb/incandescent_light_bulb.glb";
+import { GlowLayer } from "@babylonjs/core/Layers/glowLayer";
+import { PointLight } from "@babylonjs/core/Lights/pointLight";
 
 export class TestScene implements CreateSceneClass {
     createScene = async (
@@ -47,7 +53,7 @@ export class TestScene implements CreateSceneClass {
         // This creates and positions a free camera (non-mesh)
         const camera = new ArcRotateCamera(
             "arcRotateCamera",
-            0,
+            Math.PI/2,
             Math.PI/2,
             7,
             new Vector3(0, 1, 0),
@@ -76,31 +82,27 @@ export class TestScene implements CreateSceneClass {
             ".glb"
         );
 
-        const model: AbstractMesh = importResult.meshes[0];
-        // just scale it so we can see it better
-        model.scaling.scaleInPlace(7);
-        model.position = new Vector3(model.position.x, model.position.y, model.position.z+0.2);
+        let bulb: AbstractMesh = new AbstractMesh('bulbPlaceholder');
+        importResult.meshes.forEach(mesh => {
+            console.log(mesh.name);
+            mesh.scaling.scaleInPlace(4);
+
+            if(mesh.name === "Object_5")
+                bulb = mesh;
+        });
 
         //Create PBR material
         const pbr = new PBRMaterial("pbr", scene);
         pbr.metallic = 0;
-        pbr.roughness = 1;
+        pbr.roughness = 0;
         pbr.subSurface.isRefractionEnabled = true;
         pbr.subSurface.indexOfRefraction = 1.5;
-        pbr.subSurface.tintColor = Color3.Black();
+        pbr.subSurface.tintColor = Color3.White();
 
-        const torus = CreateTorusKnot("torus",{
-            radius: 1,
-            tube: 0.5,
-            radialSegments: 128,
-            tubularSegments: 128
-        },
-            scene
-        );
+        bulb.material = pbr;
 
-        torus.position.y = 1.2;
-        torus.rotation.y = Math.PI/2;
-        torus.material = pbr;
+        pbr.emissiveColor = Color3.White();
+        pbr.emissiveColor = Color3.Black();
 
         /**************************** */
         const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI('UI');
@@ -116,74 +118,78 @@ export class TestScene implements CreateSceneClass {
         userInstructions.top = '30%';
         advancedTexture.addControl(userInstructions);
         
-        // BUTTONS
-        const buttonUp = Button.CreateSimpleButton('buttonUp', '+')
-        buttonUp.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-        buttonUp.cornerRadius = 5;
-        buttonUp.width = '200px';
-        buttonUp.height = '40px';
-        buttonUp.color = 'green';
-        buttonUp.background = 'teal';
-        if(buttonUp.textBlock != undefined)
-            buttonUp.textBlock.color = 'white';
+        // // BUTTONS
+        // const buttonUp = Button.CreateSimpleButton('buttonUp', '+')
+        // buttonUp.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+        // buttonUp.cornerRadius = 5;
+        // buttonUp.width = '200px';
+        // buttonUp.height = '40px';
+        // buttonUp.color = 'green';
+        // buttonUp.background = 'teal';
+        // if(buttonUp.textBlock != undefined)
+        //     buttonUp.textBlock.color = 'white';
 
-        const buttonDown = Button.CreateSimpleButton('buttonDown', '-')
-        buttonDown.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-        buttonDown.cornerRadius = 5;
-        buttonDown.width = '200px';
-        buttonDown.height = '40px';
-        buttonDown.color = 'green';
-        buttonDown.background = 'teal';
-        if(buttonDown.textBlock != undefined)
-            buttonDown.textBlock.color = 'white';
+        // const buttonDown = Button.CreateSimpleButton('buttonDown', '-')
+        // buttonDown.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+        // buttonDown.cornerRadius = 5;
+        // buttonDown.width = '200px';
+        // buttonDown.height = '40px';
+        // buttonDown.color = 'green';
+        // buttonDown.background = 'teal';
+        // if(buttonDown.textBlock != undefined)
+        //     buttonDown.textBlock.color = 'white';
 
-        const stackPanel = new StackPanel();
-        stackPanel.isVertical = false;
-        stackPanel.spacing = 5;
-        stackPanel.addControl(buttonDown);
-        stackPanel.addControl(buttonUp);
-        stackPanel.zIndex = 1000;
-        advancedTexture.addControl(stackPanel);
+        // const stackPanel = new StackPanel();
+        // stackPanel.isVertical = false;
+        // stackPanel.spacing = 5;
+        // stackPanel.addControl(buttonDown);
+        // stackPanel.addControl(buttonUp);
+        // stackPanel.zIndex = 1000;
+        // advancedTexture.addControl(stackPanel);
         
-        // INTERACTIONS
-        let counter: number = 2;
+        // // INTERACTIONS
+        // let counter: number = 2;
         
-        const counterDisplay = new TextBlock();
-        counterDisplay.text = counter.toString();
-        counterDisplay.color = "white";
-        counterDisplay.fontSize = 56;
-        counterDisplay.top = '-20%';
-        advancedTexture.addControl(counterDisplay);
+        // const counterDisplay = new TextBlock();
+        // counterDisplay.text = counter.toString();
+        // counterDisplay.color = "white";
+        // counterDisplay.fontSize = 56;
+        // counterDisplay.top = '-20%';
+        // advancedTexture.addControl(counterDisplay);
         
-        //TODO: Do something when buttons are pressed
-        buttonDown.onPointerUpObservable.add(()=>{
-            counter--;
-            counterDisplay.text = counter.toString();
-        });
+        // //TODO: Do something when buttons are pressed
+        // buttonDown.onPointerUpObservable.add(()=>{
+        //     counter--;
+        //     counterDisplay.text = counter.toString();
+        // });
 
-        buttonUp.onPointerUpObservable.add(()=>{
-            counter++;
-            counterDisplay.text = counter.toString();
-        });
+        // buttonUp.onPointerUpObservable.add(()=>{
+        //     counter++;
+        //     counterDisplay.text = counter.toString();
+        // });
         /**************************** */
 
 
         /////////
-        // LIGHTS
+        // ENV
         /////////
-        //Directional light               
-        const dlightPosition = new Vector3(0.02, -0.05, -0.05);
-        const dLightOrientation = new Vector3(0, 20, 0);
-        const dLight = new DirectionalLight(
-            "dLight",
-            dlightPosition,
-            scene
-        );
-        dLight.intensity = 0.2;
-        dLight.position.y = 10;
 
-        //Directional light orientation
-        dLight.position = dLightOrientation;
+        // Point light
+        // const pLight = new PointLight('pLight', new Vector3(0,1,0));
+        // pLight.intensity = 10;
+        // //Directional light               
+        // const dlightPosition = new Vector3(0.02, -0.05, -0.05);
+        // const dLightOrientation = new Vector3(0, 20, 0);
+        // const dLight = new DirectionalLight(
+        //     "dLight",
+        //     dlightPosition,
+        //     scene
+        // );
+        // dLight.intensity = 0.2;
+        // dLight.position.y = 10;
+
+        // //Directional light orientation
+        // dLight.position = dLightOrientation;
 
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
         const hLight = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
@@ -198,10 +204,16 @@ export class TestScene implements CreateSceneClass {
             createGround: false,
         });
 
-        const shadowGenerator = new ShadowGenerator(512, dLight)
-        shadowGenerator.useBlurExponentialShadowMap = true;
-        shadowGenerator.blurScale = 2;
-        shadowGenerator.setDarkness(0.2);
+        const glow = new GlowLayer("glow", scene, {
+            mainTextureFixedSize: 2024,
+            blurKernelSize: 128,
+        });
+        glow.intensity = 0.5;
+
+        // const shadowGenerator = new ShadowGenerator(512, dLight)
+        // shadowGenerator.useBlurExponentialShadowMap = true;
+        // shadowGenerator.blurScale = 2;
+        // shadowGenerator.setDarkness(0.2);
 
         return scene;
     };
